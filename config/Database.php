e2460084be172dee8530c7595780aaf3d1335693
ALEX0661 <niloalexies@gmail.com>
2024-11-09 11:55:40 +0800

    Create Database.php

---
+++ config/Database.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+date_default_timezone_set("Asia/Manila");
+
+define("DB_SERVER", "localhost");
+define("DB_NAME", "Tsk_Mngmnt");
+define("DB_USER", "root");
+define("DB_PWORD", "");
+
+class Connection {
+    public function __construct() {
+        protected $connectionstring = "mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME . ";charset=utf8";
+        protected $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+    }
+
+    public function connect() {
+            return new \PDO($this->connectionstring, DB_USER, DB_PWORD, $this->options);
+        }
+    }
+}
+?>
