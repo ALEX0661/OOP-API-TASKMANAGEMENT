b6141f104fabc836be2e541ef63f922c588e1fcc
ALEX0661 <niloalexies@gmail.com>
2024-11-09 13:08:28 +0800

    Create Get.php

---
+++ modules/Get.php
@@ -0,0 +1,44 @@
+<?php
+class Get
+{
+    protected $gm;
+    protected $pdo;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->gm = new GlobalMethods($pdo);
+        $this->pdo = $pdo;
+    }
+
+    public function getTaskinfo($task_id = null)
+    {
+        $sqlString = "SELECT * FROM Tsk_Mngmnt";
+        if ($task_id !== null) {
+            $sqlString .= " WHERE task_id = " . $task_id;
+        }
+
+        $data = array();
+        $errmsg = "";
+        $code = 0;
+
+        try {
+            if ($result = $this->pdo->query($sqlString)->fetchAll()) {
+                foreach ($result as $record) {
+                    array_push($data, $record);
+                }
+                $result = null;
+                $code = 200;
+                return array("data" => $data, "code" => $code);
+            } else {
+                $errmsg = "No data found";
+                $code = 404;
+            }
+        } catch (PDOException $e) {
+            $errmsg = $e->getMessage();
+            $code = 403;
+        }
+
+        return array("code" => $code, "errmsg" => $errmsg); 
+    }
+}
+?>
