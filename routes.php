f36aa9f9177bdaddd19e1a2a3aac46254f2aea25
ALEX0661 <niloalexies@gmail.com>
2024-11-27 00:32:19 +0800

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,12 +2,18 @@
 require_once "./config/Database.php";
 require_once "./modules/Get.php";
 require_once "./modules/Post.php";
+require_once "./modules/Patch.php";
+require_once "./modules/Delete.php";
+require_once "./modules/Auth.php";
 
 $db = new Connection();
 $pdo = $db->connect();
 
-$Get = new Get($pdo);
-$Post = new Post($pdo);
+$get = new Get($pdo);
+$post = new Post($pdo);
+$patch = new Patch($pdo);
+$delete = new Delete($pdo);
+$auth = new Authentication($pdo);
 
 if (isset($_REQUEST['request'])) {
     $request = explode("/", $_REQUEST['request']);
@@ -16,60 +22,80 @@ if (isset($_REQUEST['request'])) {
     exit;
 }
 
+$body = json_decode(file_get_contents("php://input"));
+
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         switch ($request[0]) {
-            case 'gettasks':
+            case 'tasks':
                 if (count($request) > 1) {
-                    echo json_encode($Get->getTaskinfo($request[1]));
+                    echo json_encode($get->getTaskInfo($request[1]));
                 } else {
-                    echo json_encode($Get->getTaskinfo());
+                    echo json_encode($get->getTaskInfo());
                 }
                 break;
             default:
-                echo "Invalid request.";
+                http_response_code(401);
+                echo "Invalid endpoint.";
                 break;
         }
         break;
 
-/*
     case 'POST':
         switch ($request[0]) {
-            case 'posttasks':
-                if (count($request) > 1) {
-                    echo json_encode($Get->getTaskinfo($request[1]));
-                } else {
-                    echo json_encode($Get->getTaskinfo());
-                }
+            case 'login':
+                echo json_encode($auth->login($body));
                 break;
-
-            case 'updatetasks':
-                if (count($request) > 1) {
-                    echo json_encode($Get->getTaskinfo($request[1]));
-                } else {
-                    echo json_encode($Get->getTaskinfo());
-                }
+            case 'user':
+                echo json_encode($auth->addAccount($body));
                 break;
-
-
-            case 'deletetasks':
-                if (count($request) > 1) {
-                    echo json_encode($Get->getTaskinfo($request[1]));
-                } else {
-                    echo json_encode($Get->getTaskinfo());
-                }
+            case 'tasks':
+                echo json_encode($post->postTasks($body));
                 break;
-
-
             default:
-                echo "Invalid request.";
+                http_response_code(401);
+                echo "Invalid endpoint.";
                 break;
-
         }
         break;
-*/
+
+    case 'PATCH':
+        switch ($request[0]) {
+            case 'tasks':
+                if (count($request) > 1) {
+                    echo json_encode($patch->updateTask($body, $request[1]));
+                } else {
+                    http_response_code(400);
+                    echo "Task ID is required for update.";
+                }
+                break;
+            default:
+                http_response_code(401);
+                echo "Invalid endpoint.";
+                break;
+        }
+        break;
+
+    case 'DELETE':
+        switch ($request[0]) {
+            case 'tasks':
+                if (count($request) > 1) {
+                    echo json_encode($delete->deleteTask($request[1]));
+                } else {
+                    http_response_code(400);
+                    echo "Task ID is required for deletion.";
+                }
+                break;
+            default:
+                http_response_code(401);
+                echo "Invalid endpoint.";
+                break;
+        }
+        break;
+
     default:
-        echo "Forbidden";
+        http_response_code(400);
+        echo "Invalid Request Method.";
         break;
 }
 ?>
