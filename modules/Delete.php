610c7d55b6c6843a200b7823cee52d49aab07044
ALEX0661 <niloalexies@gmail.com>
2024-12-01 22:21:58 +0800

    Update Delete.php

--- modules/Delete.php
+++ modules/Delete.php
@@ -3,21 +3,48 @@ class Delete {
 
     protected $pdo;
 
-    public function __construct(\PDO $pdo) {
+    public function __construct(\PDO $pdo){
         $this->pdo = $pdo;
     }
 
-    public function deleteTask($task_id) {
+    // Delete a Campaign by ID
+    public function deleteCampaign($id){
+
         $errmsg = "";
         $code = 0;
 
         try {
-            $sqlString = "DELETE FROM task WHERE task_id = ?";
+            // Delete the campaign from the database
+            $sqlString = "DELETE FROM campaigns_tbl WHERE id = ?";
             $sql = $this->pdo->prepare($sqlString);
-            $sql->execute([$task_id]);
+            $sql->execute([$id]);
 
             $code = 200;
-            $data = "Task successfully deleted.";
+            $data = null;
+
+            return array("data" => $data, "code" => $code);
+        } catch (\PDOException $e) {
+            $errmsg = $e->getMessage();
+            $code = 400;
+        }
+
+        return array("errmsg" => $errmsg, "code" => $code);
+    }
+
+    // Delete a Pledge by ID
+    public function deletePledge($id){
+
+        $errmsg = "";
+        $code = 0;
+
+        try {
+            // Delete the pledge from the database
+            $sqlString = "DELETE FROM pledges_tbl WHERE id = ?";
+            $sql = $this->pdo->prepare($sqlString);
+            $sql->execute([$id]);
+
+            $code = 200;
+            $data = null;
 
             return array("data" => $data, "code" => $code);
         } catch (\PDOException $e) {
