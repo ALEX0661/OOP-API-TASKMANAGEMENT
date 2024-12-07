ccd8a25d8ec158b88a83082ca0c7921d7d7ab902
ALEX0661 <niloalexies@gmail.com>
2024-12-07 09:36:07 +0800

    Update Patch.php

--- modules/Patch.php
+++ modules/Patch.php
@@ -3,126 +3,107 @@ class Patch {
 
     protected $pdo;
 
-    public function __construct(\PDO $pdo){
+    public function __construct(\PDO $pdo) {
         $this->pdo = $pdo;
     }
 
-    // Update Campaign details
-    public function updateCampaign($body, $id){
+    public function patchCampaign($body, $id) {
         $values = [];
         $errmsg = "";
         $code = 0;
 
-        // Collect all values from the request body
         foreach ($body as $value) {
             array_push($values, $value);
         }
 
-        // Add the campaign ID to the values array
         array_push($values, $id);
 
         try {
-            // SQL query to update campaign details
-            $sqlString = "UPDATE campaigns_tbl SET title=?, description=?, goal=?, status=? WHERE id = ?";
+            $sqlString = "UPDATE campaigns_tbl SET title = ?, description = ?, goal_amount = ?, deadline = ? WHERE id = ?";
             $sql = $this->pdo->prepare($sqlString);
             $sql->execute($values);
 
             $code = 200;
             $data = null;
 
-            // Return the successful response
-            return array("data" => $data, "code" => $code);
+            return ["data" => $data, "code" => $code];
         } catch (\PDOException $e) {
             $errmsg = $e->getMessage();
             $code = 400;
         }
 
-        // Return the error response in case of failure
-        return array("errmsg" => $errmsg, "code" => $code);
+        return ["errmsg" => $errmsg, "code" => $code];
     }
 
-    // Archive a Campaign (soft delete)
-    public function archiveCampaign($id){
+    public function archiveCampaign($id) {
         $errmsg = "";
         $code = 0;
 
         try {
-            // SQL query to soft-delete the campaign by updating the 'isdeleted' flag
-            $sqlString = "UPDATE campaigns_tbl SET isdeleted=1 WHERE id = ?";
+            $sqlString = "UPDATE campaigns_tbl SET is_deleted = 1 WHERE id = ?";
             $sql = $this->pdo->prepare($sqlString);
             $sql->execute([$id]);
 
             $code = 200;
             $data = null;
 
-            // Return the successful response
-            return array("data" => $data, "code" => $code);
+            return ["data" => $data, "code" => $code];
         } catch (\PDOException $e) {
             $errmsg = $e->getMessage();
             $code = 400;
         }
 
-        // Return the error response in case of failure
-        return array("errmsg" => $errmsg, "code" => $code);
+        return ["errmsg" => $errmsg, "code" => $code];
     }
 
-    // Update Pledge details
-    public function updatePledge($body, $id){
+    public function patchPledge($body, $id) {
         $values = [];
         $errmsg = "";
         $code = 0;
 
-        // Collect all values from the request body
         foreach ($body as $value) {
             array_push($values, $value);
         }
 
-        // Add the pledge ID to the values array
         array_push($values, $id);
 
         try {
-            // SQL query to update pledge details
-            $sqlString = "UPDATE pledges_tbl SET amount=?, message=? WHERE id = ?";
+            $sqlString = "UPDATE pledges_tbl SET amount = ?, backer_name = ?, message = ? WHERE id = ?";
             $sql = $this->pdo->prepare($sqlString);
             $sql->execute($values);
 
             $code = 200;
             $data = null;
 
-            // Return the successful response
-            return array("data" => $data, "code" => $code);
+            return ["data" => $data, "code" => $code];
         } catch (\PDOException $e) {
             $errmsg = $e->getMessage();
             $code = 400;
         }
 
-        // Return the error response in case of failure
-        return array("errmsg" => $errmsg, "code" => $code);
+        return ["errmsg" => $errmsg, "code" => $code];
     }
 
-    // Archive a Pledge (soft delete)
-    public function archivePledge($id){
+    public function archivePledge($id) {
         $errmsg = "";
         $code = 0;
 
         try {
-            // SQL query to soft-delete the pledge by updating the 'isdeleted' flag
-            $sqlString = "UPDATE pledges_tbl SET isdeleted=1 WHERE id = ?";
+            $sqlString = "UPDATE pledges_tbl SET is_deleted = 1 WHERE id = ?";
             $sql = $this->pdo->prepare($sqlString);
             $sql->execute([$id]);
 
             $code = 200;
             $data = null;
 
-            // Return the successful response
-            return array("data" => $data, "code" => $code);
+            return ["data" => $data, "code" => $code];
         } catch (\PDOException $e) {
             $errmsg = $e->getMessage();
             $code = 400;
         }
 
-        // Return the error response in case of failure
-        return array("errmsg" => $errmsg, "code" => $code);
+        return ["errmsg" => $errmsg, "code" => $code];
     }
 }
+
 ?>
